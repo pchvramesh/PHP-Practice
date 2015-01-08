1baf35988976addeefcb4c8b7edbf41d3188e170
Ramesh Kumar <ramesh@cloudfuze.com>
2015-01-08 13:11:53 +0530

    Created ajax calls js and php files

---
+++ InvokeRestService/ajaxCalls.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ramesh
+ * Date: 1/7/2015
+ * Time: 6:30 PM
+ */?>
+<script type="text/javascript" src="js/ajaxCalls.js"></script>
---
+++ InvokeRestService/js/ajaxCalls.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ramesh on 1/7/2015.
+ */
+var ajaxCalls = {
+    loadUsers : function(){
+        $('#CF_Users_List').append('<li>Users Loaded</li>');
+        var url = "http://www.thomas-bayer.com/sqlrest/CUSTOMER/";
+        $.ajax({
+            type:"GET",
+            datatype:"JSON",
+            url:"requestHandler.php?type=usersList&url="+url,
+            success:function(data){
+                console.log(data);
+                //
+            },
+            complete:function(xhr){
+                console.log(xhr);
+            }
+        });
+    }
+};
