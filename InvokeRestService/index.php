ac589e79eb0c2471dd33b34f17cfc1f3e4386b42
Ramesh Kumar <ramesh@cloudfuze.com>
2015-01-08 13:11:12 +0530

    Added script and html code for loading user data

--- InvokeRestService/index.php
+++ InvokeRestService/index.php
@@ -1,3 +1,11 @@
+<!doctype html>
 <?php
 	require_once('JQuery.php');
 ?>
+<ul id="CF_Users_List">
+
+</ul>
+<?php require_once('ajaxCalls.php')?>
+<script type="text/javascript">
+    ajaxCalls.loadUsers();
+</script>
